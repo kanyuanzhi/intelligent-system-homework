bad0708e8724649488f8ceb1a76a6c8cd24b93d3
kanyuanzhi <kan@mail.ustc.edu.cn>
2017-10-01 21:22:43 +0800

    add BP Network

--- BPNetwokDemo.py
+++ BPNetwokDemo.py
@@ -1,26 +1,26 @@
+# -*- coding: UTF-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def tansig(x):
+def tansig(x):  # 双曲正切S型函数
     result = (1 - np.exp(-x)) / (1 + np.exp(-x))
     return result
 
 
-def dtansig(x):
+def dtansig(x):  # 双曲正切S型函数的导数
     result = 2 / (np.exp(x) + np.exp(-x) + 2)
     return result
 
 
 def catb(b):
-    new_b = np.ones((b.shape[0], 1))
-    for i in range(b.shape[0]):
-        new_b[i][0] = b[i].sum()
-    return new_b
+    new_b = np.ones((b.shape[1], 1))
+    return np.dot(b, new_b)
 
 
 def SSE(a):
-    return np.sum(a ** 2) / 2
+    return (a ** 2).sum() / 2
 
 
 p = np.arange(-1, 1.1, 0.1).reshape(1, 21)
@@ -34,13 +34,13 @@ t = np.array([[-0.96, -0.577, -0.0729, 0.377, 0.641, 0.66, 0.461, 0.1336, -0.201
 
 s1 = 5
 
-rdm = np.random.RandomState(23424)
+rdm = np.random.RandomState(1)
 w1 = 1 - 2 * rdm.random_sample((s1, 1))
 b1 = 1 - 2 * rdm.random_sample((s1, 1))
 w2 = 1 - 2 * rdm.random_sample((1, s1))
 b2 = 1 - 2 * rdm.random_sample()
 
-# w1 = 1 - 2 * np.random.random_sample((5, 1))
+# random_samplew1 = 1 - 2 * np.random.random_sample((5, 1))
 # b1 = 1 - 2 * np.random.random_sample((5, 1))
 # w2 = 1 - 2 * np.random.random_sample((1, 5))
 # b2 = 1 - 2 * np.random.random_sample()
@@ -48,8 +48,9 @@ b2 = 1 - 2 * rdm.random_sample()
 a1 = tansig(np.dot(w1, p) + b1)
 a2 = np.dot(w2, a1) + b2
 e = t - a2
+print e
 
-eta = 0.01
+eta = 0.02
 max_epoch = 100000
 error_goal = 0.01
 plt.figure('target & process')
@@ -57,10 +58,10 @@ plt.figure('target & process')
 for i in range(max_epoch):
     if (SSE(e) < error_goal):
         break
-    if (SSE(e) > 20):
-        break
-    if (i % 5000 == 0):
-        plt.plot(p[0], a2[0])
+    # if (SSE(e) > 20):
+    #     break
+    # if (i % 5000 == 0):
+    #     plt.plot(p[0], a2[0])
     dw2 = eta * np.dot(e, a1.T)
     db2 = eta * e.sum()
     w2 += dw2
@@ -80,6 +81,11 @@ for i in range(max_epoch):
 
 print SSE(e)
 print i
+print w1
+print b1
+print w2
+print b2
+print np.dot(w2, tansig(w1 * 0.15 + b1)) + b2
 
 plt.plot(p[0], t[0], '.')
 plt.plot(p[0], a2[0])
